3fb3fea6b778f3cacb7c5df25f5640bc1ef2e046
Vikkett <varennikvika@gmail.com>
2025-11-03 12:29:38 +0100

    feat: exo 3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import socksImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
 
+const inventory = ref(11)
+
+const stockStatus = computed(() => {
+  if (inventory.value > 10) {
+    return 'En stock'
+  } else if (inventory.value >= 1 && inventory.value <= 10) {
+    return 'Faible stock'
+  } else {
+    return 'En rupture de stock'
+  }
+})
 </script>
 
 <template>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <!-- image goes here -->
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-      </div>
-    </div>
+  <div>
+    <h1>{{ product }}</h1>
+    <img :src="socksImage" alt="Socks" style="width: 120px; height: auto;" />
+
+    <p>{{ stockStatus }}</p>
   </div>
 </template>
