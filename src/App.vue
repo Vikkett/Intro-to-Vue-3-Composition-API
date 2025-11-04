8ae541ddee330efe13bc27028646647d2fcf9ee6
Vikkett <varennikvika@gmail.com>
2025-11-04 11:43:00 +0100

    feat: exo7

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,71 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
 import socksBlueImage from './assets/images/socks_blue.jpeg'
 
-const product = ref('Socks')
-const image = ref(socksGreenImage)
-const inStock = true
-  
+const brand = ref("CPNV")
+const product = ref("Socks")
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
-
 const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage },
-  { id: 2235, color: 'blue', image: socksBlueImage },
+  { id: 2234, color: 'green', image: socksGreenImage, quantity: 10 },
+  { id: 2235, color: 'blue',  image: socksBlueImage,  quantity: 0 }
 ])
 
+const selectedVariant = ref(0)
 const cart = ref(0)
+const title = computed(() => brand.value + " " + product.value)
+const image = computed(() => {
+  return variants.value[selectedVariant.value].image
+})
 
-const addToCart = () => cart.value += 1
+const inStock = computed(() => {
+  return variants.value[selectedVariant.value].quantity > 0
+})
 
-const updateImage = (variantImage) => image.value = variantImage
+const addToCart = () => {
+  if (inStock.value) cart.value++
+}
 
+const updateVariant = (index) => {
+  selectedVariant.value = index
+  console.log("selectedVariant = ", selectedVariant.value)
+}
 </script>
-  
+
+
 <template>
   <div class="nav-bar"></div>
   <div class="cart">Cart({{ cart }})</div>
+  
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
+      <div class="product-image">
+        <img :src="image" />
       </div>
       <div class="product-info">
-        <h1>{{ product }}</h1>
+        <h1>{{ title }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
         <ul>
-          <li v-for="detail in details">{{ detail }}</li>
+          <li v-for="detail in details" :key="detail">
+            {{ detail }}
+          </li>
         </ul>
-        <div v-for="variant in variants" 
+        <div 
+          v-for="(variant, index) in variants"
           :key="variant.id"
-          @mouseover="updateImage(variant.image)"
+          @mouseover="updateVariant(index)"
         >
           {{ variant.color }}
         </div>
-        <button class="button" v-on:click="addToCart">Add to cart</button>
+        <button 
+          class="button" 
+          @click="addToCart"
+          :disabled="!inStock"
+        >
+          Add to cart
+        </button>
+
       </div>
     </div>
   </div>
